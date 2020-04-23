c56d853422d810b7e7277f9247e2d302aa065486
gebamp <georgebampilis@gmail.com>
2020-04-23 22:07:29 +0300

    3.1

---
+++ Lab_3/Lab_3/Lab_3.sim/sim_1/behav/compile.bat
@@ -0,0 +1,12 @@
+@echo off
+rem  Vivado(TM)
+rem  compile.bat: a Vivado-generated XSim simulation Script
+rem  Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+
+set PATH=%XILINX%\lib\%PLATFORM%;%XILINX%\bin\%PLATFORM%;C:/Xilinx/SDK/2014.2/bin;C:/Xilinx/Vivado/2014.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2014.2/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2014.2/bin;%PATH%
+set XILINX_PLANAHEAD=C:/Xilinx/Vivado/2014.2
+
+xelab -m64 --debug typical --relax -L xil_defaultlib -L secureip --snapshot FIR_TB_behav --prj C:/Users/Superminiala/Documents/VLSI/Lab_3/Lab_3.sim/sim_1/behav/FIR_TB.prj   xil_defaultlib.FIR_TB
+if errorlevel 1 (
+   cmd /c exit /b %errorlevel%
+)
